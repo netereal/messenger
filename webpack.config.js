abddd7f8e3b52c582792d66cfc771a2044b7f45b
netereal <novik.maria@gmail.com>
2019-07-04 02:35:23 +0300

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: "./dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.sass/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin('./src/index.html')
+    ]
+};
