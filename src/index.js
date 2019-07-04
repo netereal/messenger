4fd03e767175b3199942092bd330c77bcabed2c1
netereal <novik.maria@gmail.com>
2019-07-04 03:47:08 +0300

    configure loaders for pug/njk

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
 import _ from 'lodash';
-import './app.sass';
-
-function component() {
-    const element = document.createElement('div');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
+import './sass/app.sass';
